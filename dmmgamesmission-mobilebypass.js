476239e77abcd12a076384c6302cd8fb70df23bb
pandamon <mizuniji@gmail.com>
2025-05-01 14:11:59 +0800

    更新5月手机端游戏

--- dmmgamesmission-mobilebypass.js
+++ dmmgamesmission-mobilebypass.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMM Games Mission - Mobile UA Bypass
 // @namespace    https://www.youtube.com/watch?v=dQw4w9WgXcQ
-// @version      beta-0.0.2
+// @version      beta-0.0.3
 // @description  Bypass some dmm mobile game check userAgent in browser
 // @author       Pandamon
 // @match        https://play.games.dmm.com/game/yuusyananteokotowari_150692
@@ -10,6 +10,8 @@
 // @match        https://play.games.dmm.com/game/otogi_f_sp
 // @match        https://play.games.dmm.co.jp/game/inyouchu-kin_sp
 // @match        https://play.games.dmm.co.jp/game/oenshinshix_sp
+// @match        https://play.games.dmm.co.jp/game/otogi_f_r_sp
+// @match        https://play.games.dmm.co.jp/game/bokuchin
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @run-at       document-start
 // @grant        unsafeWindow
